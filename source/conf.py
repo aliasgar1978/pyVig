805eb3e136c4ac81eb89f4f02e12dcf98d02b88c
aliasgar1978 <aholo2000@gmail.com>
2022-02-10 16:12:19 +0530

    UPDATE

--- source/conf.py
+++ source/conf.py
@@ -23,7 +23,7 @@ copyright = '2022, ALIASGAR'
 author = 'ALIASGAR'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.2'
+release = '0.0.4'
 
 
 # -- General configuration ---------------------------------------------------
