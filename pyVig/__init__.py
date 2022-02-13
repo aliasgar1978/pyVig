078c6e6315e9281208474ab5194e136527f4cae0
aliasgar1978 <aholo2000@gmail.com>
2022-02-13 10:22:32 +0530

    .

--- pyVig/__init__.py
+++ pyVig/__init__.py
@@ -16,9 +16,9 @@ __ver__ = "0.0.5"
 
 # ------------------------------------------------------------------------------
 
+from .stencils import get_list_of_stencils
 from .visio import VisioObject
 from .entities import ItemObjects, Connectors
-from .stencils import get_list_of_stencils
 from .database import DeviceData, CableMatrixData
 from .gui import UserForm
 # ------------------------------------------------------------------------------
