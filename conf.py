5924ca43be084ff078ce50fc3dbd68251e204eb2
aliasgar1978 <aholo2000@gmail.com>
2023-04-06 08:24:14 +0530

    .

--- conf.py
+++ conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
@@ -37,7 +37,7 @@ extensions = [
     'sphinx.ext.autosummary',
     'sphinx.ext.coverage', 
     'sphinx.ext.napoleon',
-    # 'sphinx.ext.viewcode',
+    'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
     'sphinx_rtd_theme',
 ]
